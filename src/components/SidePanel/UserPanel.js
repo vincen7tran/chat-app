4c3e3dacfaa377e7295d8300cc55b153ec22dad4
Vinh Tran <vincen7.tran@gmail.com>
2019-07-28 15:58:05 -0700

    avatar displays

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import firebase from '../../firebase';
-import { connect } from 'react-redux';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import { Grid, Header, Icon, Dropdown, Image } from 'semantic-ui-react';
 
 class UserPanel extends React.Component {
   state = {
     user: this.props.currentUser
-  }
-
-  componentWillReceiveProps(nextProps) {
-    console.log(nextProps);
-    this.setState({ user: nextProps.currentUser });
   }
 
   dropdownOptions = () => {
@@ -41,7 +35,7 @@ class UserPanel extends React.Component {
   }
 
   render() {
-    const { displayName } = this.state.user;
+    const { displayName, photoURL } = this.state.user;
 
     return (
       <Grid style={{ background: '#4c3c4c' }}>
@@ -52,26 +46,21 @@ class UserPanel extends React.Component {
               <Icon name="code" />
               <Header.Content>DevChat</Header.Content>
             </Header>
-          </Grid.Row>
 
-          {/* User Dropdown */}
-          <Header style={{ padding: '0.25em' }} as="h4" inverted>
-            <Dropdown trigger={
-              <span>{displayName}</span>
-            } options={this.dropdownOptions()} />
-          </Header> 
+            {/* User Dropdown */}
+            <Header style={{ padding: '0.25em' }} as="h4" inverted>
+              <Dropdown trigger={
+                <span>
+                  <Image src={photoURL} spaced="right" avatar />
+                  {displayName}
+                </span>
+              } options={this.dropdownOptions()} />
+            </Header> 
+          </Grid.Row>
         </Grid.Column>
       </Grid>
     );
   }
 }
 
-const mapStateToProps = state => {
-  const { currentUser } = state.user;
-
-  return {
-    currentUser
-  };
-};
-
-export default connect(mapStateToProps)(UserPanel);
+export default UserPanel;
