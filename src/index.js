f53bf8f378f582ed5f2ab7cdd9a4aa43880d6e07
Vinh Tran <vincen7.tran@gmail.com>
2019-07-28 12:31:26 -0700

    redux setup

--- src/index.js
+++ src/index.js
@@ -5,18 +5,28 @@ import Login from './components/Auth/Login';
 import Register from './components/Auth/Register';
 import registerServiceWorker from './registerServiceWorker';
 import firebase from './firebase';
+import rootReducer from './reducers';
 
 import 'semantic-ui-css/semantic.min.css';
 
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider, connect } from 'react-redux';
+import { setUser } from './actions';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers());
 
 class Root extends React.Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        this.props.history.push('/');
+        const { setUser, history } = this.props;
+
+        setUser(user);
+        history.push('/');
       }
-    })
+    });
   }
 
   render() {
@@ -30,11 +40,13 @@ class Root extends React.Component {
   }
 }
 
-const RootWithAuth = withRouter(Root);
+const RootWithAuth = withRouter(connect(null, { setUser })(Root));
 
 ReactDOM.render(
-<Router>
-  <RootWithAuth />
-</Router>,
+<Provider store={store}>
+  <Router>
+    <RootWithAuth />
+  </Router>
+</Provider>,
 document.getElementById('root'));
 registerServiceWorker();
